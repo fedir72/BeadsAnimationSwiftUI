5c863cbbe6b5bcba4735bb31a2537e02b181fa96
fedir72 <fedir72@ua.fm>
2022-06-28 18:18:10 +0300

    empty project

---
+++ BeadsCounter/BeadsCounter/BeadsCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeadsCounterApp.swift
+//  BeadsCounter
+//
+//  Created by Fedii Ihor on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct BeadsCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
